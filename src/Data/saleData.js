1a344e5b1dd05691978915f01c67f4e8090557c6
cmoore134 <cmoore134@student.gsu.edu>
2026-07-26 21:37:14 -0400

    Create saleData.js

---
+++ src/Data/saleData.js
@@ -0,0 +1,7 @@
+export const salesRecords = [
+  { Sid: "SALE001", id: "INV001", SVid: "SER001", category: "Dairy", name: "Milk", quantity: 24, sale_date: "2026-01-20", sale_amount: 48.00 },
+  { Sid: "SALE002", id: "INV002", SVid: "SER002", category: "Produce", name: "Bananas", quantity: 50, sale_date: "2026-07-15", sale_amount: 100.00 },
+  { Sid: "SALE003", id: "INV003", SVid: "SER003", category: "Bakery", name: "Bread", quantity: 15, sale_date: "2026-07-18", sale_amount: 30.00 },
+  { Sid: "SALE004", id: "INV004", SVid: "SER004", category: "Meat", name: "Chicken Breast", quantity: 30, sale_date: "2026-07-16", sale_amount: 90.00 },
+  { Sid: "SALE005", id: "INV005", SVid: "SER005", category: "Beverages", name: "Orange Juice", quantity: 40, sale_date: "2026-08-01", sale_amount: 80.00 },
+];
